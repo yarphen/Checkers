fc53355c076b6c15361df7b37c3a9cf664af9f58
yarphen <leviatan.ka14@gmail.com>
2016-10-06 23:52:03 +0300

    Way of states

---
+++ src/main/java/com/fishteam/checkers/logics/Way.java
@@ -0,0 +1,14 @@
+package com.fishteam.checkers.logics;
+
+public class Way {
+	private GameState [] states;
+
+	public GameState[] getStates() {
+		return states;
+	}
+
+	public void setStates(GameState[] states) {
+		this.states = states;
+	}
+
+}
