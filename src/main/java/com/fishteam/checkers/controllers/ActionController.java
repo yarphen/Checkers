9401b658945f4ae237f7b7f16ecfdeaafe7ea419
yarphen <leviatan.ka14@gmail.com>
2016-10-06 23:50:06 +0300

    Creating sample actions

--- src/main/java/com/fishteam/checkers/controllers/ActionController.java
+++ src/main/java/com/fishteam/checkers/controllers/ActionController.java
@@ -1,11 +1,39 @@
 package com.fishteam.checkers.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fishteam.checkers.interfaces.ProblemSolver;
+import com.fishteam.checkers.logics.Action;
+import com.fishteam.checkers.logics.Game;
+import com.fishteam.checkers.logics.Way;
+
 
 /**
  * This is controller to interact with js on page
  */
 @Controller
 public class ActionController {
-
+	@Autowired
+//	@Qualifier("gameMap")
+	private Map<String, Game> gameMap;
+	@Autowired
+//	@Qualifier("solverMap")
+	private Map<String, ProblemSolver> solverMap;
+	@RequestMapping(value="/game",method = RequestMethod.PUT)
+	public @ResponseBody Game newGame(){
+		Game game = new Game();
+		gameMap.put(game.getId(), game);
+		return game;
+	}
+	@RequestMapping(value="/game",method = RequestMethod.POST)
+	public Way solveGame(Action action){
+		return gameMap.get(action.getId()).solveWith(solverMap.get(action.getAlgorythm()));
+	}
 }
