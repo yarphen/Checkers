df2660dc09bec42bd995dab0639f093a28009035
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2016-10-07 10:33:38 +0300

    front-end initialize

--- src/main/java/com/fishteam/checkers/controllers/ActionController.java
+++ src/main/java/com/fishteam/checkers/controllers/ActionController.java
@@ -1,11 +1,45 @@
 package com.fishteam.checkers.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fishteam.checkers.interfaces.ProblemSolver;
+import com.fishteam.pacman.json.GameInfo;
+
+
 
 /**
  * This is controller to interact with js on page
  */
 @Controller
 public class ActionController {
+//	@Autowired
+//	private GameMap queueMap;
+	@Autowired
+	private ProblemSolver bfs_solver;
+	@Autowired
+	private ProblemSolver dfs_solver;
+	
+	@RequestMapping(value="/game",method = RequestMethod.PUT)
+	public void createGame(int id){
+		
+	}
+	
+	@RequestMapping(value="/board",method = RequestMethod.POST)
+	public void board(int id){
+		
+	}
+	
+	@RequestMapping(value="/active",method = RequestMethod.POST)
+	public void active(int id){
+		
+	}
+	
+	@RequestMapping(value="/solve",method = RequestMethod.POST)
+	public void solve(int id){
+		
+	}
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,32 @@
 <html>
+<head>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+	
+</script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.0.0/pixi.min.js">
+	
+</script>
+<script type="text/javascript" src="resources/scripts.js">
+	
+</script>
+<link rel="stylesheet" href="resources/styles.css" />
+</head>
 <body>
-<h2>Hello World!</h2>
+	<div class="container">
+		<div class="content">
+			<div class="title">
+				<h2>Checkers</h2>
+			</div>
+			<div class="layer-wrapper">
+				<div class="layer">
+					<div class="game"></div>
+				</div>
+				<div class="layer">
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
