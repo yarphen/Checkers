01b09039eab6878d9e94bdce913bb38ac40ab6fb
yarphen <leviatan.ka14@gmail.com>
2016-10-09 22:18:26 +0300

    Fixing stupid error, add logging

--- src/main/java/com/fishteam/checkers/controllers/ActionController.java
+++ src/main/java/com/fishteam/checkers/controllers/ActionController.java
@@ -2,19 +2,15 @@
 package com.fishteam.checkers.controllers;
 
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fishteam.checkers.interfaces.MapContainer;
-import com.fishteam.checkers.interfaces.ProblemSolver;
 import com.fishteam.checkers.logics.Action;
 import com.fishteam.checkers.logics.Board;
 import com.fishteam.checkers.logics.Checker;
@@ -37,32 +33,45 @@ public class ActionController {
 	public @ResponseBody Game newGame(){
 		Game game = new Game();
 		mapContainer.getGameMap().put(game.getId(), game);
+		System.out.println("PUT /game");
+		System.out.println();
 		return game;
 	}
 	@RequestMapping(value="/game/{id}",method = RequestMethod.DELETE)
-	public void removeGame(@RequestParam(value="id") String id){
+	public @ResponseBody String removeGame(@PathVariable(value="id") String id){
 		mapContainer.getGameMap().remove(id);
+		System.out.println("DELETE /game/"+id);
+		System.out.println();
+		return "";
 	}
 
 	@RequestMapping(value="/board/{id}",method = RequestMethod.POST)
-	public void board(@RequestParam(value="id") String id, @RequestBody Action action){
+	public @ResponseBody String board(@PathVariable(value="id") String id, @RequestBody Action action){
 		Game game = mapContainer.getGameMap().get(id);
 		GameState state = (GameState)game.getStartState();
 		Board board = state.getBoard();
 		board.setCell(action.getX(), action.getY(), action.getCheckerState());
+		System.out.println("POST /board/"+id);
+		System.out.println("Action: "+action);
+		System.out.println();
+		return "";
 	}
 	@RequestMapping(value="/target/{id}",method = RequestMethod.POST)
-	public void target(@RequestParam(value="id") String id, @RequestBody Action action){
+	public @ResponseBody String target(@PathVariable(value="id") String id, @RequestBody Action action){
 		Game game = mapContainer.getGameMap().get(id);
 		GameState state = (GameState)game.getGoalState();
 		GameState startState = (GameState)game.getStartState();
 		Checker checker = state.getMainChecker();
 		checker.setColor(startState.getMainChecker().isColor());
 		checker.setLocation(new Point(action.getX(), action.getY()));
+		System.out.println("POST /target/"+id);
+		System.out.println("Action: "+action);
+		System.out.println();
+		return "";
 	}
 
 	@RequestMapping(value="/active/{id}",method = RequestMethod.POST)
-	public void active(@RequestParam(value="id") String id, @RequestBody Action action){
+	public @ResponseBody String active(@PathVariable(value="id") String id, @RequestBody Action action){
 		Game game = mapContainer.getGameMap().get(id);
 		GameState state = (GameState)game.getStartState();
 		Checker checker = state.getMainChecker();
@@ -77,11 +86,20 @@ public class ActionController {
 			break;
 		}
 		state.setMainChecker(checker);
+		System.out.println("POST /active/"+id);
+		System.out.println("Action: "+action);
+		System.out.println();
+		return "";
 	}
 
 	@RequestMapping(value="/solve/{id}",method = RequestMethod.POST)
-	public Way solveGame(@RequestParam(value="id") String id, @RequestBody Action action){
-		return mapContainer.getGameMap().get(id)
-				.solveWith(mapContainer.getSolverMap().get(action.getAlgorythm()));
+	public @ResponseBody Way solveGame(@PathVariable(value="id") String id, @RequestBody Action action){
+		Way w = mapContainer.getGameMap().get(id)
+				.solveWith(mapContainer.getSolverMap().get(action.getAlgorithm()));
+
+		System.out.println("POST /solve/"+id);
+		System.out.println("Action: "+action);
+		System.out.println();
+		return w;
 	}
 }
